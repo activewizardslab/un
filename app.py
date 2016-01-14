836af98480fed620f27b6155cebcb3671c346476
taras-zak <taras.zakan@gmail.com>
2016-01-14 13:59:43 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify, url_for, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
+
+@app.route('/handle', methods=['POST'])
+def handle():
+    form = request.form
+    print form
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
