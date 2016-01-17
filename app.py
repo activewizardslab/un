ba0d4a1863329337a1a485fe1744f224ff8aad3e
taras-zak <taras.zakan@gmail.com>
2016-01-17 18:57:28 +0200

    add form validation

--- app.py
+++ app.py
@@ -1,20 +1,50 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_wtf import Form
+from wtforms import SelectField, IntegerField
+from wtforms.validators import input_required, none_of
 
 app = Flask(__name__)
+app.config.from_object('config')
 
-@app.route('/')
+class ChooseForm(Form):
+    relation = SelectField('Type of relation',
+        coerce=int,
+        choices=[
+            (0, 'Type of relation'),
+            (1, 'Item 1'),
+            (2, 'Item 2'),
+            (3, 'Item 3')],
+        default=0,
+        validators=[
+            input_required(message="Please, choose type of relation"),
+            none_of([0], message="Please, choose type of relation")
+            ])
+    category = SelectField(
+        coerce=int,
+        choices=[(0, 'Category'),
+                 (1, 'Item 1'),
+                 (2, 'Item 2'),
+                 (3, 'Item 3')],
+        default=0,
+        validators=[
+            input_required(message="Please, choose category"),
+            none_of([0], message="Please, choose category")
+            ])
+    messages_number = IntegerField('Number of messages',
+        validators=[
+            input_required(message="Please, choose number of messages")])
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('main.html')
+    form = ChooseForm()
+    if request.method == 'POST' and form.validate():
+        flash("Good")
+        return redirect(url_for('index'))
+    return render_template('main.html', form=form)
 
-# @app.route('/graph')
-# def graph():
-#     return render_template('graph2.html')
-
-@app.route('/handle', methods=['POST'])
-def handle():
-    form = request.form
-    print form
-    return redirect(url_for('index'))
+@app.route('/graph')
+def graph():
+    return render_template('graph2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
